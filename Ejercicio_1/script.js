71d039eea3f13101c66bf700e61442eed5a78123
Lucas Chanquia <lucaschanquia@gmail.com>
2021-07-01 18:49:44 -0300

    modificacion valores js

---
+++ Ejercicio_1/script.js
@@ -0,0 +1,13 @@
+let limite = 25;
+
+console.log('Números hasta límite:')
+
+     for ( let i = 0; i <= limite; i++ ) {
+     console.log(i) 
+    }  
+    
+console.log("Numeros pares:")
+     for ( let i = 0; i <= limite; i++ ) {
+        if (i % 2 ===0) { 
+            console.log(i)}}
+            
